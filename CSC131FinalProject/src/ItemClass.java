38bbdddadac56eb68ca01ed907caa14dfa528f1c
aqibau <49211446+aqibau@users.noreply.github.com>
2019-05-04 21:04:44 -0700

    created ItemClass

--- CSC131FinalProject/src/ItemClass.java
+++ CSC131FinalProject/src/ItemClass.java
@@ -1,4 +1,18 @@
+public class ItemClass
+{
+	public static int Main()
+	{
+		int CellID;
+		String CellName;
+		int cellCoordinateX;
+		int cellCoordinateY;
 
-public class ItemClass {
+		CellID = 1234;
+		CellName = Phone;
+		cellCoordinateX = 0;
+		cellCoordinateY = 0;
 
+
+		return 0;
+	}
 }
