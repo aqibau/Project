3ef76ba32eadd99549889bb695a448da49a2892b
Suraj <Suraj@192.168.254.43>
2019-05-07 16:34:35 -0700

    new commit

--- CSC131FinalProject/src/ServerClass.java
+++ CSC131FinalProject/src/ServerClass.java
@@ -223,7 +223,7 @@ public class ServerClass
 		}
 		catch(FileNotFoundException e)
 		{
-			System.out.println("Error opening file itemList.txt"); 
+			System.out.println("Error opening file itemList.txt"); // create text file
 			System.exit(0);
 		}
 		
